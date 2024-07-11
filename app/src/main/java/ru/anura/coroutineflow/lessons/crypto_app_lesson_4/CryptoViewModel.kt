51043f23748f504c8470a363cd457b16a1cf4f29
anura1402 <ann99909@gmail.com>
2024-07-11 18:15:19 +0300

    Extension function mergeWith for blocking button during init and loading

--- app/src/main/java/ru/anura/coroutineflow/lessons/crypto_app_lesson_4/CryptoViewModel.kt
+++ app/src/main/java/ru/anura/coroutineflow/lessons/crypto_app_lesson_4/CryptoViewModel.kt
@@ -9,10 +9,12 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
@@ -23,15 +25,21 @@ class CryptoViewModel : ViewModel() {
 
     private val repository = CryptoRepository
 
-
+    private val loadingFlow = MutableSharedFlow<State>()
 
     val state: Flow<State> = repository.getCurrencyList()
         .filter { it.isNotEmpty() }
         .map { State.Content(currencyList = it) as State }
         .onStart { emit(State.Loading) }
+        .mergeWith(loadingFlow)
+
+    private fun <T> Flow<T>.mergeWith(another: Flow<T>):Flow<T>{
+        return merge(this,another)
+    }
 
     fun refreshList() {
         viewModelScope.launch {
+            loadingFlow.emit(State.Loading)
             repository.refreshList()
         }
     }
