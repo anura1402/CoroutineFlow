d85ee6af3225e7a358f3e8a54da782bd98848545
anura1402 <ann99909@gmail.com>
2024-07-12 17:41:24 +0300

    Buffer and Backpressure in HotFlow

--- app/src/main/java/ru/anura/coroutineflow/lessons/lesson8/Lesson8.kt
+++ app/src/main/java/ru/anura/coroutineflow/lessons/lesson8/Lesson8.kt
@@ -4,27 +4,36 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
 
 import kotlinx.coroutines.launch
 
 suspend fun main(){
     val scope = CoroutineScope(Dispatchers.Default)
-    val job = scope.launch {
-        val flow: Flow<Int> = flow {
-            repeat(10){
-                println("Emitting $it")
-                emit(it)
-                println("Emit done $it")
-                delay(200)
-            }
-        }.buffer() //размер 64 элемента, при переполнении корутина приостанавливается, пока буффер не освободится
-        flow.collect{
+
+    val flow = MutableSharedFlow<Int>()
+
+    val producer = scope.launch {
+        delay(500)
+        repeat(10){
+            println("Emitting $it")
+            flow.emit(it)
+            println("Emit done $it")
+            delay(200)
+        }
+    }
+
+    val consumer = scope.launch {
+        flow.collectLatest{
             println("Collected $it")
             delay(1000)
         }
     }
-    job.join()
+
+    producer.join()
+    consumer.join()
 
 }
---
+++ app/src/main/java/ru/anura/coroutineflow/lessons/lesson9/Lesson9.kt
@@ -0,0 +1,30 @@
+package ru.anura.coroutineflow.lessons.lesson8
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.flow
+
+import kotlinx.coroutines.launch
+
+suspend fun main(){
+    val scope = CoroutineScope(Dispatchers.Default)
+    val job = scope.launch {
+        val flow: Flow<Int> = flow {
+            repeat(10){
+                println("Emitting $it")
+                emit(it)
+                println("Emit done $it")
+                delay(200)
+            }
+        }.buffer() //размер 64 элемента, при переполнении корутина приостанавливается, пока буффер не освободится
+        flow.collect{
+            println("Collected $it")
+            delay(1000)
+        }
+    }
+    job.join()
+
+}
