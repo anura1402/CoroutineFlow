17e0e3a6050665c2d131b3b930f68778abaa0c26
anura1402 <ann99909@gmail.com>
2024-07-12 17:42:18 +0300

    Fixed lessons

--- app/src/main/java/ru/anura/coroutineflow/lessons/lesson8/Lesson8.kt
+++ app/src/main/java/ru/anura/coroutineflow/lessons/lesson8/Lesson8.kt
@@ -4,36 +4,27 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.buffer
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
-
 import kotlinx.coroutines.launch
+
 
 suspend fun main(){
     val scope = CoroutineScope(Dispatchers.Default)
-
-    val flow = MutableSharedFlow<Int>()
-
-    val producer = scope.launch {
-        delay(500)
-        repeat(10){
-            println("Emitting $it")
-            flow.emit(it)
-            println("Emit done $it")
-            delay(200)
-        }
-    }
-
-    val consumer = scope.launch {
-        flow.collectLatest{
+    val job = scope.launch {
+        val flow: Flow<Int> = flow {
+            repeat(10){
+                println("Emitting $it")
+                emit(it)
+                println("Emit done $it")
+                delay(200)
+            }
+        }.buffer() //размер 64 элемента, при переполнении корутина приостанавливается, пока буффер не освободится
+        flow.collect{
             println("Collected $it")
             delay(1000)
         }
     }
-
-    producer.join()
-    consumer.join()
+    job.join()
 
 }
